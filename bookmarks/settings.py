a94e2a36b515b1533ae28dbc96f91650133fc503
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-05-03 21:30:16 -0300

    chore: set account at the top of INSTALLED_APPS variable to avoid using some default auth functionalities.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -31,13 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # account added at the top -
+    # to avoid using some built-in-
+    # auth authentication.
+    'account', 
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
+    
 ]
 
 MIDDLEWARE = [
