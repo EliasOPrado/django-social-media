a7b3b0b89dc4ad2d41fbd03bef16c3923ddc7f6d
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-05-11 01:02:38 -0300

    chore: Transformed the Action model into a generic/polymorphic model.

--- actions/models.py
+++ actions/models.py
@@ -1,5 +1,7 @@
 import re
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class Action(models.Model):
@@ -9,6 +11,15 @@ class Action(models.Model):
                             on_delete=models.CASCADE)
     # the action name.
     verb= models.CharField(max_length=255)
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True, 
+                                  null=True, 
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE,)
+    target_id = models.PositiveIntegerField(null=True,
+                                           blank=True, 
+                                           db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
