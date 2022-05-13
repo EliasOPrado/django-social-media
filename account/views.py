ff5567e79b0b7e24dc85e381eb5a735775ac5594
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-05-12 21:40:04 -0300

    chore: fixed actions.

--- account/views.py
+++ account/views.py
@@ -1,19 +1,58 @@
-from re import M
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import LoginForm, UserRegistrationForm, \
-                   UserEditForm, ProfileEditForm
-from django.http import JsonResponse
-from actions.utils import create_action
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
-from .models import Profile, Contact
+from actions.utils import create_action
 from actions.models import Action
+from .models import Profile, Contact
+from .forms import LoginForm, UserRegistrationForm, \
+                   UserEditForm, ProfileEditForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated '\
+                                        'successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'actions': actions})
+
 
 def register(request):
     if request.method == 'POST':
@@ -39,54 +78,11 @@ def register(request):
                   {'user_form': user_form})
 
 
-def user_login(request):
-    
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated Successfully')
-
-                else:
-                    return HttpResponse('Disabled Account!')
-
-            else:
-                return HttpResponse('Invalid Login!')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form':form})
-
-
-@login_required
-def dashboard(request):
-    # show all action by default.
-    actions = Action.objects.exclude(user=request.user)
-    following_ids = request.user.following.values_list('id', flat=True)
-    if following_ids:
-        # if the user is follwing other users,
-        # get only its actions.
-        actions = actions.filter(user_id__in=following_ids)
-    actions = actions.select_related('user', 'user__pofile')\
-                    .prefetch_related('target')[:10]
-                    
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard',
-                  'actions':actions})
-
 @login_required
 def edit(request):
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user,
                                  data=request.POST)
-        
         profile_form = ProfileEditForm(
                                     instance=request.user.profile,
                                     data=request.POST,
@@ -111,14 +107,18 @@ def user_list(request):
     users = User.objects.filter(is_active=True)
     return render(request,
                   'account/user/list.html',
-                   {'section':'people',
-                   'users':users})
+                  {'section': 'people',
+                   'users': users})
 
 @login_required
 def user_detail(request, username):
-    user = get_object_or_404(User, username=username,
+    user = get_object_or_404(User,
+                             username=username,
                              is_active=True)
-    return render(request, 'account/user/detail.html', {'section':'people', 'user':user})
+    return render(request,
+                  'account/user/detail.html',
+                  {'section': 'people',
+                   'user': user})
 
 
 @ajax_required
@@ -127,8 +127,6 @@ def user_detail(request, username):
 def user_follow(request):
     user_id = request.POST.get('id')
     action = request.POST.get('action')
-
-    # checks if both from above are in.
     if user_id and action:
         try:
             user = User.objects.get(id=user_id)
@@ -142,4 +140,4 @@ def user_follow(request):
             return JsonResponse({'status':'ok'})
         except User.DoesNotExist:
             return JsonResponse({'status':'error'})
-    return JsonResponse({'status':'error'})   
+    return JsonResponse({'status':'error'})
--- actions/utils.py
+++ actions/utils.py
@@ -3,21 +3,21 @@ from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
 from .models import Action
 
+
 def create_action(user, verb, target=None):
-    # checks if there is any similar action-
-    # fired in the last minute.
+    # check for any similar action made in the last minute
     now = timezone.now()
     last_minute = now - datetime.timedelta(seconds=60)
-    similar_actions = Action.objects.filter(user_id=user.id, verb=verb,
-                                            created__gte=last_minute)
+    similar_actions = Action.objects.filter(user_id=user.id,
+                                       verb= verb,
+                                       created__gte=last_minute)
     if target:
         target_ct = ContentType.objects.get_for_model(target)
         similar_actions = similar_actions.filter(
-            target_ct=target_ct,
-            target_id=target.id
-        )
+                                             target_ct=target_ct,
+                                             target_id=target.id)
     if not similar_actions:
-        # no existing action found.
+        # no existing actions found
         action = Action(user=user, verb=verb, target=target)
         action.save()
         return True
