9b10c933a1b0d294461900213984233715cd5506
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-05-12 23:47:38 -0300

    chore: Implemented image ranking.

--- images/views.py
+++ images/views.py
@@ -1,5 +1,3 @@
-import redis
-from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -7,15 +5,20 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
-from actions.utils import create_action
 from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
 from common.decorators import ajax_required
+from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
 
-# redis connection.
-r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+import redis
+from django.conf import settings
+
+# connect to redis
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 @login_required
@@ -47,13 +50,15 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    # increments by 1 the total vizualization of the image.
-    total_views = r.incr(f'image: {image.id}: views')
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image,
-                   'total_views':total_views})
+                   'total_views': total_views})
 
 
 @ajax_required
@@ -71,7 +76,7 @@ def image_like(request):
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status':'ok'})
-        except Error:
+        except:
             pass
     return JsonResponse({'status':'error'})
 
@@ -79,7 +84,7 @@ def image_like(request):
 @login_required
 def image_list(request):
     images = Image.objects.all()
-    paginator = Paginator(images, 8)
+    paginator = Paginator(images, 1)
     page = request.GET.get('page')
     try:
         images = paginator.page(page)
@@ -100,3 +105,18 @@ def image_list(request):
     return render(request,
                   'images/image/list.html',
                    {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(
+                           id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
